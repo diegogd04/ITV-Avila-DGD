591b0aaaac33bbadf9fdc000b2f5a98b5806f00f
diegogd04 <diego2612004@gmail.com>
2022-12-15 18:58:25 +0100

    clase inspeccion

---
+++ src/com/dgd/ITV/domain/Inspeccion.java
@@ -0,0 +1,49 @@
+package com.dgd.ITV.domain;
+
+public class Inspeccion {
+    private Integer condigoInspeccion;
+    private String fechaInspeccion;
+    private Boolean resultadoInspeccion;
+    private Vehiculo vehiculo;
+    private Propietario propietario;
+
+    public Integer getCondigoInspeccion() {
+        return condigoInspeccion;
+    }
+
+    public void setCondigoInspeccion(Integer condigoInspeccion) {
+        this.condigoInspeccion = condigoInspeccion;
+    }
+
+    public String getFechaInspeccion() {
+        return fechaInspeccion;
+    }
+
+    public void setFechaInspeccion(String fechaInspeccion) {
+        this.fechaInspeccion = fechaInspeccion;
+    }
+
+    public Boolean getResultadoInspeccion() {
+        return resultadoInspeccion;
+    }
+
+    public void setResultadoInspeccion(Boolean resultadoInspeccion) {
+        this.resultadoInspeccion = resultadoInspeccion;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Propietario getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Propietario propietario) {
+        this.propietario = propietario;
+    }
+}
